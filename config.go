58e452ae7f9c6e93426fc76db5c3486587d7435d
Gary Chang <gwh.chang@gmail.com>
2015-09-02 17:10:42 -0500

    default config

--- config.go
+++ config.go
@@ -4,6 +4,7 @@ import (
     "log"
     "flag"
     "os"
+    "strings"
     "io/ioutil"
     "gopkg.in/yaml.v2"
 )
@@ -30,8 +31,10 @@ func LoadConfig() *Config {
 
     // 2nd Priority: YAML Config
     source, err := ioutil.ReadFile(*configFile)
+    hasConfig := true
     if err != nil {
         log.Printf("Could not locate %s. Running without config...",*configFile)
+        hasConfig = false
     }
     err = yaml.Unmarshal(source, config)
     if err != nil {
@@ -46,10 +49,14 @@ func LoadConfig() *Config {
         config.Group = *group
     }
     if *mode != "" {
-        config.Mode = *mode
+        config.Mode = strings.ToLower(*mode)
     }
     if *help {
         config.Help = true
+    }
+
+    if !hasConfig {
+        LoadDefault(config)
     }
 
     // Check if config is legal
@@ -65,3 +72,25 @@ func LoadConfig() *Config {
 func (c *Config) ok() bool {
     return c.Key != "" && c.Group != "" && c.Mode != "";
 }
+
+func LoadDefault(config *Config) {
+    switch config.Mode {
+    case "top":
+        LoadDefaultTop(config)
+    default:
+    }
+}
+
+func LoadDefaultTop(config *Config) {
+    defaults := []string{
+        "cpu_usage_user",
+        "cpu_usage_sys",
+        "cpu_usage_idle"}
+    for _, s := range defaults {
+        config.Variables = append(config.Variables, Variable{
+            Pattern: s,
+            Description: s})
+    }
+}
+
+
