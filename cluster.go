3b9fbae195c4de294e63c1bca19d60c7fabc83a8
Gary Chang <gwh.chang@gmail.com>
2015-09-06 17:27:10 -0500

    tab fix

--- cluster.go
+++ cluster.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-	"bufio"
-	"io"
-	"log"
-	"os"
-	"os/exec"
+    "bufio"
+    "io"
+    "log"
+    "os"
+    "os/exec"
 )
 
 type (
-	SyncEvent struct {
-		Time        int64
-		SeriesID    string
-		Key         string
-		Value       string
-	}
-	Event struct {
-		Time int64
-		Data map[string]interface{}
-	}
-	Series struct {
-		ID     string
-		Events []Event
-	}
-	Cluster struct {
-		Key       string
-		ID        string
-		Group     string
-		Token     string
-		Series    map[string]*Series
-	}
+    SyncEvent struct {
+        Time        int64
+        SeriesID    string
+        Key         string
+        Value       string
+    }
+    Event struct {
+        Time int64
+        Data map[string]interface{}
+    }
+    Series struct {
+        ID     string
+        Events []Event
+    }
+    Cluster struct {
+        Key       string
+        ID        string
+        Group     string
+        Token     string
+        Series    map[string]*Series
+    }
 )
 
 func StartCluster(config *Config, events chan SyncEvent) *Cluster {
-	cluster := &Cluster{
-		Key:   config.Key,
-		ID:    HashID(config.Key),
-		Group: config.Group,
-		Token: HashKey(config.Key),
+    cluster := &Cluster{
+        Key:   config.Key,
+        ID:    HashID(config.Key),
+        Group: config.Group,
+        Token: HashKey(config.Key),
         Series: make(map[string]*Series),
     }
 
-	log.Printf(">>> STARTING CLUSTER (API: %s) (GROUP: %s) (TOKEN: %s)\n\n", cluster.Key, cluster.Group, cluster.Token)
+    log.Printf(">>> STARTING CLUSTER (API: %s) (GROUP: %s) (TOKEN: %s)\n\n", cluster.Key, cluster.Group, cluster.Token)
 
     data := make(chan string)
-	go Read(config, data)
+    go Read(config, data)
 
     handlerInfo := &HandlerInfo{Cluster: cluster, Config: config, Data: data, Events: events}
     handler := GetHandler(handlerInfo)
     go handler.Run()
 
-	return cluster
+    return cluster
 }
 
 func Read(cfg *Config, data chan string) {
@@ -75,13 +75,13 @@ func Read(cfg *Config, data chan string) {
 }
 
 func ReadToBuffer(reader io.Reader, data chan string) {
-	r := bufio.NewReader(reader)
+    r := bufio.NewReader(reader)
 
-	for {
-		line,err := r.ReadString('\n')
-		data <- line
-		if err != nil && err != io.EOF {
-			log.Fatal(err)
-		}
-	}
+    for {
+        line,err := r.ReadString('\n')
+        data <- line
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
+    }
 }
